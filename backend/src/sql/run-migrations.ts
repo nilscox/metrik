0b5fc1967fc4db59de8eaa50b853256602a5f767
nils <nils@epita.fr>
2022-01-23 13:29:51 +0100

    dont load .env when running migrations in production

--- backend/src/sql/run-migrations.ts
+++ backend/src/sql/run-migrations.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import dotenv from 'dotenv-safe';
 
-dotenv.config();
-
 import { DatabaseModule } from '~/common/database/database.module';
 import { DatabaseService } from '~/common/database/database.service';
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config();
+}
 
 async function runMigrations() {
   const app = await NestFactory.createApplicationContext(DatabaseModule);
